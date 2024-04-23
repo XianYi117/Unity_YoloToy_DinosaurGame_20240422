1ab004067b1a2ae6ad1f822e20074ded045f151e
XianYi117 <149284412+XianYi117@users.noreply.github.com>
2024-04-23 16:53:17 +0800

    Update 場景銜接並移動

---
+++ Assets/Scripts/ControlSystem.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class ControlSystem : MonoBehaviour
+{
+    Rigidbody2D rb;
+    [Header("跳躍力度"), Range(0, 10)]
+    public float jump;
+    bool isJumping;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //按下Space按鍵即可跳躍並判斷是否落地
+        if(Input.GetKeyDown(KeyCode.Space)&&isJumping==false)
+        {
+            
+            rb.velocity = new Vector2 ( 0, jump );
+            isJumping = true;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isJumping = false;
+    }
+}
---
+++ Assets/Scripts/MovementSystem.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class MovementSystem : MonoBehaviour
+{
+    [Header("場景移動速度"), Range(0,10)]
+    public float movementSpeed;
+    [Header("場景重生位置"), Tooltip("場景消失後重新生成的位置")]
+    public float startPosition;
+    [Header("場景消失位置"), Tooltip("場景移動後卸載的位置")]
+    public float endPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position=new Vector2( transform.position.x -movementSpeed * Time.deltaTime, transform.position.y );
+
+        if( transform.position.x <= endPosition) 
+        {
+            transform.position = new Vector2( startPosition,transform.position.y );
+        }
+    }
+}
