77a0a7e50555ecb321f7099cbd63230e0fd8d1b5
XianYi117 <149284412+XianYi117@users.noreply.github.com>
2024-04-25 11:48:13 +0800

    Update 補充說明

--- Assets/Scripts/SpawnSystem.cs
+++ Assets/Scripts/SpawnSystem.cs
@@ -4,16 +4,19 @@ namespace Xian
 {
     public class SpawnSystem : MonoBehaviour
     {
+        //怪物陣列
         public GameObject[] monster;
+        [Header("預製物生成位置")]
         public GameObject spawnPosition;
-        public GameObject nowMonster;
+        [Header("怪物生成時間"),Tooltip("當遊戲計時大於生成時間會生成一隻怪物")]
         public float spawnTime;
+        //計時
         float timer;
 
         void Update()
         {
             timer += Time.deltaTime;
-
+            //隨機生成怪物
             int random = Random.Range(0, monster.Length);
             if (timer >= spawnTime)
             {
