1c716aebb6eac21e39bc275f4337b85fdf1ad02e
XianYi117 <149284412+XianYi117@users.noreply.github.com>
2024-04-28 11:42:44 +0800

    Update GAS連動

---
+++ Assets/Scripts/PostGoogleScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PostGoogleScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(PostData());
+    }
+
+    IEnumerator PostData()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("name", 100);
+        //form.AddField("欄位命名", "欄位資料"); 要幾種資料就要輸入幾行，並且命名，命名是為了給Google Apps Script讀取
+
+        using (UnityWebRequest www = UnityWebRequest.Post("https://script.google.com/macros/s/AKfycby5GuMpVVMLm7WwyX1kTkSO9JJfVEYEty5ZVpxd6dPeJCNn4qW1z86IFZW7QgKsvkbF/exec", form))
+        {
+            yield return www.SendWebRequest();
+        }
+    }
+}
